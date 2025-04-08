af28585e3c0d4f3a4a176010678b9ef06d0af5e9
Nolan Reilly <nolanreilly02@gmail.com>
2025-04-08 13:45:16 -0500

    Add message viewing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("node:path");
 
+const messages = require("./models/db");
+
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -14,6 +16,14 @@ const newRouter = require("./routes/newRouter");
 
 app.use("/", indexRouter);
 app.use("/new", newRouter);
+
+app.get("/:username", (req, res) => {
+  const { username } = req.params;
+
+  const userMessages = messages.filter((message) => message.user == username);
+
+  res.render("messages", { username: username, messages: userMessages });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
